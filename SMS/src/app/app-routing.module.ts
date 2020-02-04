b2fb9f111bfd99682f41b832b264e0c4155d0396
824zzy <614594368@qq.com>
2020-02-04 14:08:35 -0600

    fix($app-routing.module): fix unknown extra codes

--- SMS/src/app/app-routing.module.ts
+++ SMS/src/app/app-routing.module.ts
@@ -2,15 +2,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full'},
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  },
   {
     path: '',
     loadChildren: () => import('./index/index.module').then(m => m.IndexPageModule)
